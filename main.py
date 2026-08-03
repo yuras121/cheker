21a30aa97e12605fae093d3d54a18938fbe81306
Mark Undezberx <yurasananas@gmail.com>
2026-08-03 21:50:45 +0300

    Set up bot with database initialization
    Initialize database and set up bot polling.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
+import handlers
+import database
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    
+    # 1. Ініціалізуємо базу даних (створюємо таблиці)
+    await database.init_db()
+    print("🗄 База данных подключена!")
+    
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    
+    dp.include_router(handlers.router)
+    
+    print("🚀 PRO Бот успешно запущен!")
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
